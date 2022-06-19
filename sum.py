8bd085b1a6dbe527c3145b6d3b67dfa4a9d273e7
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-19 20:34:33 +0530

    Create sum.py
    This is a basic python program that calculates addition of two numbers

---
+++ sum.py
@@ -0,0 +1,8 @@
+n1=10
+n2=15
+sum=n1+n2
+print("Sum of {0} and {1} is {2} ".format(n1,n2,sum))
+num1 = input("Enter the First num:\n")
+num2 = input("Enter the sedcond num:\n")
+sum=float(num1)+float(num2)
+print("Sum of {0} and {1} is {2}".format(num1,num2,sum))
