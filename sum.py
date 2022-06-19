d391387f54c8f393a65df1236d4892ed91747bcc
Yash Kolekar <71895020+WhyAsh5114@users.noreply.github.com>
2022-06-19 22:34:07 +0530

    QOL changes
    Added spaces before operands,
    .format() can be replaced by f" ", much smaller and easier to read syntax
    Added input for first numbers

--- sum.py
+++ sum.py
@@ -1,8 +1,9 @@
-n1=10
-n2=15
-sum=n1+n2
-print("Sum of {0} and {1} is {2} ".format(n1,n2,sum))
-num1 = input("Enter the First num:\n")
-num2 = input("Enter the sedcond num:\n")
-sum=float(num1)+float(num2)
-print("Sum of {0} and {1} is {2}".format(num1,num2,sum))
+n1 = int(input("Enter first num: "))
+n2 = int(input("Enter second num: "))
+sum = n1 + n2
+print(f"{n1} + {n2} = {sum}")
+
+num1 = input("Enter the first num: ")
+num2 = input("Enter the second num: ")
+sum = float(num1) + float(num2)
+print(f"Sum of {num1} and {num2} is {sum}")
