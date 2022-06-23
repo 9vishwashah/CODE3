69692e70f798ae442696b2f72f4112371a5d5fb4
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-23 09:30:12 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,14 @@
+l = []
+num = int(input("number of elements you want to enter in list: "))
+for i in range(1, num + 1):
+	a= int(input("{0}st element: ".format(i)))
+	l.append(a)
+print("Positive Numbers from {0} to {1} are:".format(1,i))
+for n in l:
+  if (n >= 0):
+   print(n, end = " ")
+  elif (n<0):
+   print("There are no positive numbers")
+   break
+  else:
+   print("Invalid Input")
