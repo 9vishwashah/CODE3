677b3abfe4c972913b3ae51d1a5855b1cf84c38c
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-23 18:13:37 +0530

    Create negative_range.py
    This is a program that prints negative elements from entered number by the user.

---
+++ negative_range.py
@@ -0,0 +1,9 @@
+l = []
+num = int(input("number of elements you want to enter in list: "))
+for i in range(1, num + 1):
+  a= int(input("{0}st element: ".format(i)))
+  l.append(a)
+print("Negative Numbers from {0} to {1} are:".format(1,i))
+for n in l:
+  if (n < 0):
+    print(n, end = " ")
