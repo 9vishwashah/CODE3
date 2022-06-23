baa072df4a9f5a5fa0b2e22d556bc2a7f806ca20
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-23 08:59:09 +0530

    Create even_range.py
    This is a program that displays even number according to range entered by the user.

---
+++ even_range.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the start of range: "))
+b = int(input("Enter the end of range: "))
+print("Even Numbers from {0} to {1} are:".format(a,b))
+for n in range(a, b + 1):
+	if n % 2 == 0:
+		print(n, end = " ")
