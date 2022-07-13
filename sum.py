be2a8990dc634a8bcf2d22e3af5655026ac2d4e1
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-13 08:49:44 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,9 +1,4 @@
-n1 = int(input("Enter first num: "))
-n2 = int(input("Enter second num: "))
-sum = n1 + n2
-print(f"{n1} + {n2} = {sum}")
-
-num1 = input("Enter the first num: ")
-num2 = input("Enter the second num: ")
-sum = float(num1) + float(num2)
-print(f"Sum of {num1} and {num2} is {sum}")
+a = float(input("Enter first num: "))
+b = float(input("Enter second num: "))
+c = a + b
+print(f"{a} + {b} = {c}")
