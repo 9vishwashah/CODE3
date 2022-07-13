dc5094870af7b7fe595cee0d82894614a8b5de8b
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-13 08:51:24 +0530

    Update word_search_paragraph.py

--- word_search_paragraph.py
+++ word_search_paragraph.py
@@ -1,8 +1,8 @@
 def check(s2, s1):
 	if (s2.count(s1) > 0):
-		print("{0} Word is present in the paragraph".format(s1))
+		print(f"{s1} Word is present in the paragraph")
 	else:
-		print("{0} Word is present in the paragraph".format(s1))
+		print(f"{s1} Word is present in the paragraph")
 s2 = input("Enter the paragraph:\n")
 s1 = input("Enter a word you want to search:\n")
 check(s2, s1)
