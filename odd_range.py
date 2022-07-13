3ce527e96872f7a12668538a31fa81e03af901a4
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-13 08:33:43 +0530

    Update odd_range.py

--- odd_range.py
+++ odd_range.py
@@ -1,6 +1,6 @@
 a = int(input("Enter the start of range: "))
 b = int(input("Enter the end of range: "))
-print("Odd Numbers from {0} to {1} are:".format(a,b))
+print(f"Odd Numbers from {a} to {b} are:")
 for n in range(a, b + 1):
 	if n % 2 != 0:
 		print(n, end = " ")
