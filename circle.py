f1ee3f78401ccf63601a26f025d1df432c1ec9c6
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-13 08:21:46 +0530

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,6 +1,6 @@
 r = float (input ("Enter the Radius:\n"))  
 a = 3.1415926535897932*r*r
-c = 2*pi*r
+c = 2*3.1415926535897932*r
 d = 2*r
 print("The Area of the given circle:",end="")
 print (round(a,3))
